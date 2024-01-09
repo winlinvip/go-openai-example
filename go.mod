c2a60aadaad52ebdc51184e7ec7c5e6ca2345829
winlin <winlinvip@gmail.com>
2024-01-09 16:51:38 +0800

    Update Tencent ASR example.

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module app
 go 1.18
 
 require (
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/ossrs/go-oryx-lib v0.0.9 // indirect
 	github.com/sashabaranov/go-openai v1.17.7 // indirect
+	github.com/tencentcloud/tencentcloud-speech-sdk-go v1.0.13 // indirect
+	golang.org/x/net v0.17.0 // indirect
 )
---
+++ tencent_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/ossrs/go-oryx-lib/errors"
+	"github.com/tencentcloud/tencentcloud-speech-sdk-go/asr"
+	"github.com/tencentcloud/tencentcloud-speech-sdk-go/common"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var AppID, SecretID, SecretKey string
+
+func init() {
+	if err := godotenv.Load(".env.tencent"); err != nil {
+		panic(err)
+	}
+
+	AppID = os.Getenv("APPID")
+	SecretID = os.Getenv("SECRET_ID")
+	SecretKey = os.Getenv("SECRET_KEY")
+}
+
+// MySpeechRecognitionListener implementation of SpeechRecognitionListener
+type MySpeechRecognitionListener struct {
+}
+
+func (listener *MySpeechRecognitionListener) OnRecognitionStart(response *asr.SpeechRecognitionResponse) {
+}
+
+func (listener *MySpeechRecognitionListener) OnSentenceBegin(response *asr.SpeechRecognitionResponse) {
+}
+
+func (listener *MySpeechRecognitionListener) OnRecognitionResultChange(response *asr.SpeechRecognitionResponse) {
+}
+
+func (listener *MySpeechRecognitionListener) OnSentenceEnd(response *asr.SpeechRecognitionResponse) {
+	fmt.Printf("ASR result: %s\n", response.Result.VoiceTextStr)
+}
+
+func (listener *MySpeechRecognitionListener) OnRecognitionComplete(response *asr.SpeechRecognitionResponse) {
+}
+
+func (listener *MySpeechRecognitionListener) OnFail(response *asr.SpeechRecognitionResponse, err error) {
+}
+
+func Test_Tencent_ASR(t *testing.T) {
+	EngineModelType, inputFile, inputFormat := "16k_zh", "./test.wav", asr.AudioFormatWav
+	if err := func() error {
+		recognizer := asr.NewSpeechRecognizer(
+			AppID, common.NewCredential(SecretID, SecretKey),
+			EngineModelType, &MySpeechRecognitionListener{},
+		)
+		recognizer.VoiceFormat = inputFormat
+		if err := recognizer.Start(); err != nil {
+			return errors.Wrapf(err, "recognizer start failed")
+		}
+		defer recognizer.Stop()
+
+		audio, err := os.Open(inputFile)
+		if err != nil {
+			return errors.Wrapf(err, "open file error")
+		}
+		defer audio.Close()
+
+		if b, err := ioutil.ReadFile(inputFile); err != nil {
+			return errors.Wrapf(err, "read file error")
+		} else {
+			if err = recognizer.Write(b); err != nil {
+				return errors.Wrapf(err, "recognizer write error")
+			}
+		}
+
+		return nil
+	}(); err != nil {
+		t.Errorf("test error: %v", err)
+	}
+}
